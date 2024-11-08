95833605166e2df969b161b5295c4e8caaf3a218
valeriaaa12 <valeriaromero1275@gmail.com>
2024-11-08 17:04:45 -0600

    Otro repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import NavScrollExample from './navbar/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <NavScrollExample/>
+  );
+}
+
+export default App;
